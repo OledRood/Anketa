91363cde854bd2d84e7905797d47833cf0b9de2f
OledRood <96789787+OledRood@users.noreply.github.com>
2023-11-13 20:10:03 +0300

    Add files via upload

---
+++ Anketa/main.js
@@ -0,0 +1,9 @@
+var vody = document.querySelector('body');
+
+
+const menuBtn = document.querySelector(".menu__btn");
+const menuList = document.querySelector(".menu__list");
+function toggleMenuVisibility() {
+menuList.classList.toggle("hide");
+}
+ 
