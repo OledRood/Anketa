bde8f0cc5cd5d0dcd44d7b627884452d6b4f0666
OledRood <96789787+OledRood@users.noreply.github.com>
2023-11-13 20:30:27 +0300

    Delete Anketa directory

--- Anketa/main.js
+++
@@ -1,9 +0,0 @@
-var vody = document.querySelector('body');
-
-
-const menuBtn = document.querySelector(".menu__btn");
-const menuList = document.querySelector(".menu__list");
-function toggleMenuVisibility() {
-menuList.classList.toggle("hide");
-}
- 
